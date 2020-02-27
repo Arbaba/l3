273350a88c88d5f1d15f9d8b2f57f9c75b13f98c
arbaba <bakarsadio@gmail.com>
2020-02-27 18:02:59 +0100

    Init repo

--- compiler/src/l3/CL3ToCPSTranslator.scala
+++ compiler/src/l3/CL3ToCPSTranslator.scala
@@ -4,6 +4,15 @@ import l3.{ SymbolicCL3TreeModule => S }
 import l3.{ SymbolicCPSTreeModule => C }
 
 object CL3ToCPSTranslator extends (S.Tree => C.Tree) {
-  def apply(tree: S.Tree): C.Tree =
-    ???
+  def apply(tree: S.Tree): C.Tree = 
+    transform(tree){ v : C.Atom => 
+    //(halt 0)
+    C.Halr(C.Atom(IntLit(L3Int(0))))
+  }
+  private def transform(tree: S.Tree)(ctx: C.Atom =>  C.Tree) : C.Tree = tree match {
+    case Ident(name) => 
+      ctx(c.AtomN(name))
+    case Lit(v) => 
+      ctx(c.AtomL(v))
+  }
 }
