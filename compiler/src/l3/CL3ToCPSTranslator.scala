becd99f85f01224cac9b91f1b46e60eaf0c46ac0
Frédéric Gessler <frederic.gessler@epfl.ch>
2020-02-29 13:55:19 +0100

    Finish implementing Halt and If

--- compiler/src/l3/CL3ToCPSTranslator.scala
+++ compiler/src/l3/CL3ToCPSTranslator.scala
@@ -20,11 +20,35 @@ object CL3ToCPSTranslator extends (S.Tree => C.Tree) {
           C.LetP(n1, L3Id, Seq(v1), transform(S.Let(otherArgs, e))(ctx))
         }
       }
-      case S.Let(Seq(), e) => transform(e)(ctx)
+      case S.Let(Seq(), e) => transform(e)
       //if-node with a primitive cond
-      //case S.If() => 
+      //L3TestPrimitive
+      case S.If(S.Prim(p: L3TestPrimitive, Seq(e1)), e2, e3) => {
+        val r = Symbol.fresh("r")
+        val c = Symbol.fresh("c")
+        val ct = Symbol.fresh("ct")
+        val cf = Symbol.fresh("cf")
+        C.LetC(
+          Seq(C.Cnt(c, Seq(r), ctx(C.AtomN(r)))), 
+          C.LetC(
+            Seq(C.Cnt(ct, Seq(), transform(e2){ (v2: C.Atom) => 
+              C.AppC(c, Seq(v2))
+            })), C.LetC(
+                Seq(C.Cnt(cf, Seq(), transform(e3){ (v3: C.Atom) => 
+                  C.AppC(c, Seq(v3))
+                }
+                )), transform(e1) { v1: C.Atom => 
+                  C.If(p, Seq(v1), ct, cf)
+                }
+              )
+            )
+          )
+      }
       //if-node sugared
+      //
       case S.If(e1, e2, e3) => transform(S.If(S.Prim(L3Eq, Seq(e1, S.Lit(BooleanLit(false)))), e3, e2))
+      //halt(e) => halt()
+      case S.Halt(e) => transform(e){v: C.Atom => C.Halt(v)}
     }
   }
 }
