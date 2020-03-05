9a1e04ed048d1e8fd88a8142bd5b3568bbcab342
Boubacar Camara <bakarsadio@gmail.com>
2020-03-05 14:35:42 +0100

    Implement let, letrec and Primitive translation Need to fix compilation errors

--- compiler/src/l3/CL3ToCPSTranslator.scala
+++ compiler/src/l3/CL3ToCPSTranslator.scala
@@ -21,6 +21,29 @@ object CL3ToCPSTranslator extends (S.Tree => C.Tree) {
         }
       }
       case S.Let(Seq(), e) => transform(e)(ctx)
+      case S.LetRec(funs, body ) => 
+        val c = Symbol.fresh("c")
+      
+        val fs  = funs.map(f => C.Fun(f.name, c, f.args, transform(f.body){v: C.Atom => C.AppC(c, Seq(v))})) 
+      
+        C.LetF(fs, transform(body){v: C.Atom => C.AppC(c, Seq(v))})
+      case S.App(f, args) => 
+        transform(f){v :C.Atom => 
+          
+          val (c, r) = (Symbol.fresh("c"), Symbol.fresh("r"))
+
+          val appStacker =  (ei, C.LetC(x, AppF(cnt, seq))) : (S.Tree, C.LetC) =>transform(ei){vi => C.LetC(AppF(cnt, vi::seq))}
+          args.foldRight(C.LetC(  Seq(Cnt(c, r, C.AtomN(r))),Appf(v, c , Seq())))(appStacker)
+        }
+      case S.Prim(S.L3TestPrimitive(name, arity), args) => C.If(L3TestPrimitive(Symbol.fresh(name), arity), args,  Lit(BooleanLit(true)), Lit(BooleanLit(false)))
+      case S.Prim(S.L3Primitive(name, arity)  , args) =>
+          val f = Symbol.fresh("f")
+          val c = Symbol.fresh("c")
+          val newArgs = args.map(transform)
+          C.LetP(f, C.L3TestPrimitve(name, arity), newArgs, C.AppC(c, newArgs ))
+          
+
     }
+
   }
 }
