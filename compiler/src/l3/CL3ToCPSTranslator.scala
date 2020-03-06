ff5ca33473608ece1888ea2ec8a68544ce921ab2
Frédéric Gessler <frederic.gessler@epfl.ch>
2020-03-06 15:56:03 +0100

    Fix divergence errors

--- compiler/src/l3/CL3ToCPSTranslator.scala
+++ compiler/src/l3/CL3ToCPSTranslator.scala
@@ -10,15 +10,21 @@ object CL3ToCPSTranslator extends (S.Tree => C.Tree) {
       C.Halt(C.AtomL(IntLit(L3Int(0))))
     }
   }
+  var cnt = 0
   private def transform(tree: S.Tree)(implicit ctx: C.Atom =>  C.Tree) : C.Tree = {
     implicit val pos = tree.pos
+    //println(tree.toString.toCharArray.take(100).mkString(""))
     tree match {
       //f(name, args, body)
       //case S.LetRec(f, e) => C.LetF(, transform(e))
-      case p: S.Prim => transform{
-        S.If(p, S.Lit(BooleanLit(true)), S.Lit(BooleanLit(true)))
+      case S.Prim(p: L3TestPrimitive, args) => {
+        println("transforming logiaal primitive")
+        transform{
+        S.If(S.Prim(p, args), S.Lit(BooleanLit(true)), S.Lit(BooleanLit(true)))
       }(ctx)
+      }
       case S.Prim(prim: L3ValuePrimitive, args) => {
+        println("transforming value primitive")
         val primEval = Symbol.fresh("v")
         val id = Symbol.fresh("id")
         val idRet = Symbol.fresh("id")
@@ -41,12 +47,15 @@ object CL3ToCPSTranslator extends (S.Tree => C.Tree) {
         }
       }
       case S.LetRec(funs, body ) => 
+        println("transforming letrec")
         val c = Symbol.fresh("c")
       
         val fs  = funs.map(f => C.Fun(f.name, c, f.args, transform(f.body){v: C.Atom => C.AppC(c, Seq(v))})) 
       
         C.LetF(fs, transform(body){v: C.Atom => C.AppC(c, Seq(v))})
-      case S.App(e, args) => transform(e) { v: C.Atom =>
+      case S.App(e, args) => {
+        println("transforming app")
+        transform(e) { v: C.Atom =>
         val c = Symbol.fresh("c")
         val r = Symbol.fresh("r")
         val z: C.Tree = C.LetC(Seq(C.Cnt(c, Seq(r), ctx(C.AtomN(r)))), C.AppF(v, c, Seq()))
@@ -56,41 +65,50 @@ object CL3ToCPSTranslator extends (S.Tree => C.Tree) {
           }
         }
       }
+      }
       case S.Ident(name) => ctx(C.AtomN(name))
       case S.Lit(v) => ctx(C.AtomL(v))
       case S.Let(Seq((n1, e1), otherArgs @ _*), e) => {
+        println("transforming let1")
         transform(e1){ v1: C.Atom =>
           C.LetP(n1, L3Id, Seq(v1), transform(S.Let(otherArgs, e))(ctx))
         }
       }
-      case S.Let(Seq(), e) => transform(e)
+      case S.Let(Seq(), e) => 
+      println("transforming let2")
+      transform(e)
       //if-node with a primitive cond
       //L3TestPrimitive
       
-      case S.If(S.Prim(p: L3TestPrimitive, Seq(e1)), e2, e3) => {
+      case S.If(S.Prim(p: L3TestPrimitive, e), e2, e3) => {
+        println("transforming logical if")
         val r = Symbol.fresh("r")
         val c = Symbol.fresh("c")
         val ct = Symbol.fresh("ct")
         val cf = Symbol.fresh("cf")
-        C.LetC(
-          Seq(C.Cnt(c, Seq(r), ctx(C.AtomN(r)))), 
-          C.LetC(
-            Seq(C.Cnt(ct, Seq(), transform(e2){ (v2: C.Atom) => 
-              C.AppC(c, Seq(v2))
-            })), C.LetC(
-                Seq(C.Cnt(cf, Seq(), transform(e3){ (v3: C.Atom) => 
-                  C.AppC(c, Seq(v3))
-                }
-                )), transform(e1) { v1: C.Atom => 
-                  C.If(p, Seq(v1), ct, cf)
-                }
-              )
-            )
-          )
+        val e1 = e(0)
+        transform(e2){ v2: C.Atom => 
+          transform(e3){ v3: C.Atom => 
+            val condCnt = C.Cnt(c, Seq(r), ctx(C.AtomN(r)))
+            val thenCnt = C.Cnt(ct, Seq(), C.AppC(c, Seq(v2)))
+            val elseCnt = C.Cnt(cf, Seq(), C.AppC(c, Seq(v3)))
+            C.LetC(Seq(condCnt), 
+              C.LetC(Seq(thenCnt), 
+                C.LetC(Seq(elseCnt), 
+                  transform(e1){v1: C.Atom => 
+                    C.If(p, Seq(v1), ct, cf)
+                  })))
+          }
+        }
       }
       //if-node sugared
       //
-      case S.If(e1, e2, e3) => transform(S.If(S.Prim(L3Eq, Seq(e1, S.Lit(BooleanLit(false)))), e3, e2))
+      case S.If(e1, e2, e3) => {
+        println(s"Transforming if $tree")
+        cnt += 1
+        if(cnt > 3) throw new Exception("stack overflow")
+        transform(S.If(S.Prim(L3Eq, Seq(e1, S.Lit(BooleanLit(false)))), e3, e2))
+      }
       //halt(e) => halt()
       case S.Halt(e) => transform(e){v: C.Atom => C.Halt(v)}
     }
